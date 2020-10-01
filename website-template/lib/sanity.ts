4fc7c8458cf1af419e42a802d0669e38c46bc679
Alex Bruns <alex.h.bruns@gmail.com>
2020-09-30 23:37:10 -0500

    AHB :: build debug

--- website-template/lib/sanity.ts
+++ website-template/lib/sanity.ts
@@ -3,17 +3,29 @@ import { SiteConfiguration } from "../@types/sanity";
 export const groq = String.raw;
 
 export async function execQuery<T>(query: string): Promise<T> {
-  return (
-    await (
-      await fetch(
-        `https://${
-          process.env.SANITY_PROJECT_ID
-        }.apicdn.sanity.io/v1/data/query/${
-          process.env.SANITY_DATASET_NAME
-        }?query=${encodeURIComponent(query)}`
-      )
-    ).json()
-  ).result;
+  return await fetch(
+    `https://${process.env.SANITY_PROJECT_ID}.apicdn.sanity.io/v1/data/query/${
+      process.env.SANITY_DATASET_NAME
+    }?query=${encodeURIComponent(query)}`
+  )
+    .then((r) => r.json())
+    .then((j) => j.result)
+    .then((result) => {
+      console.info(result);
+      return result;
+    });
+
+  //   await (
+
+  //     await fetch(
+  //       `https://${
+  //         process.env.SANITY_PROJECT_ID
+  //       }.apicdn.sanity.io/v1/data/query/${
+  //         process.env.SANITY_DATASET_NAME
+  //       }?query=${encodeURIComponent(query)}`
+  //     )
+  //   ).json()
+  // ).result;
 }
 
 export async function getSiteConfiguration(): Promise<SiteConfiguration> {
