e4086c9452d369821c1f17243d4cc47229077142
Alex Bruns <alex.h.bruns@gmail.com>
2020-10-02 23:34:32 -0500

    AHB :: build debug

--- website-template/lib/sanity.ts
+++ website-template/lib/sanity.ts
@@ -3,21 +3,21 @@ import { SiteConfiguration } from "../@types/sanity";
 export const groq = String.raw;
 
 export async function execQuery<T>(query: string): Promise<T> {
-  console.log(
-    `https://${process.env.SANITY_PROJECT_ID}.apicdn.sanity.io/v1/data/query/${
-      process.env.SANITY_DATASET_NAME
-    }?query=${encodeURIComponent(query)}`
-  );
+  // console.log(
+  //   `https://${process.env.SANITY_PROJECT_ID}.apicdn.sanity.io/v1/data/query/${
+  //     process.env.SANITY_DATASET_NAME
+  //   }?query=${encodeURIComponent(query)}`
+  // );
 
-  console.log(
-    await fetch(
-      `https://${
-        process.env.SANITY_PROJECT_ID
-      }.apicdn.sanity.io/v1/data/query/${
-        process.env.SANITY_DATASET_NAME
-      }?query=${encodeURIComponent(query)}`
-    )
-  );
+  // console.log(
+  //   await fetch(
+  //     `https://${
+  //       process.env.SANITY_PROJECT_ID
+  //     }.apicdn.sanity.io/v1/data/query/${
+  //       process.env.SANITY_DATASET_NAME
+  //     }?query=${encodeURIComponent(query)}`
+  //   )
+  // );
 
   console.log(
     await (
